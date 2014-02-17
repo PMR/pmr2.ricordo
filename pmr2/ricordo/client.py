2541e84b514fdc39f0916cd8c100bc9ada137e31
Tommy Yu <tommy.yu@auckland.ac.nz>
2014-02-18 08:33:26 +1300

    Example RICORDO web service usage code and tests.

---
+++ pmr2/ricordo/client.py
@@ -0,0 +1,68 @@
+import requests
+import json
+
+
+class Client(object):
+
+    def __init__(self, root,
+            host='127.0.0.1',
+            port='8080',
+            requests_session=None,
+        ):
+
+        self.host = host
+        self.port = port
+        self.root = root
+
+        if requests_session is None:
+            requests_session = requests.Session()
+            requests_session.headers.update({
+                'Accept': 'application/json',
+                'Content-type': 'application/json',
+            })
+        self.requests_session = requests_session
+
+    @property
+    def target(self):
+        if not hasattr(self, '_target'):
+            self._target = 'http://%s:%s/%s' % (
+                self.host, self.port, self.root)
+        return self._target
+
+    def get(self, endpoint, query):
+        target = '%s/%s/%s' % (self.target, endpoint, query)
+        return self.requests_session.get(target).json()
+
+    def post(self, endpoint, data):
+        target = '%s/%s' % (self.target, endpoint)
+        return self.requests_session.post(target, data=json.dumps(data)).json()
+
+
+class OwlkbClient(Client):
+    """
+    Owl knowledgebase client.
+    """
+
+    def __init__(self, root='/ricordo-owlkb-ws/service', *a, **kw):
+        super(OwlkbClient, self).__init__(root, *a, **kw)
+
+    def query_terms(self, query):
+        return self.get(query=query, endpoint='terms')
+
+
+class RdfStoreClient(Client):
+    """
+    RICORDO RDF Store client.
+    """
+
+    def __init__(self, root='/ricordo-rdfstore-ws/service', *a, **kw):
+        super(RdfStoreClient, self).__init__(root, *a, **kw)
+
+    def search(self, target, data):
+        return self.post(endpoint='search/' + target, data=data)
+
+    def getResourceForAnnotation(self, data):
+        return self.search(target='getResourceForAnnotation', data=data)
+
+    def getAnnotationOfResource(self, data):
+        return self.search(target='getAnnotationOfResource', data=data)
---
+++ pmr2/ricordo/tests/test_client.py
@@ -0,0 +1,70 @@
+import unittest
+import requests
+import json
+
+from pmr2.ricordo.client import OwlkbClient
+from pmr2.ricordo.client import RdfStoreClient
+
+
+def test_available(text, url):
+    try:
+        r = requests.get('http://127.0.0.1:8080/ricordo-owlkb-ws/')
+        return 'RICORDO-owlkb-webservice' in r.text
+    except:
+        return False
+
+def owlkb_test_available():
+    return test_available('RICORDO-owlkb-webservice',
+        'http://127.0.0.1:8080/ricordo-owlkb-ws/')
+
+def rdfstore_test_available():
+    return test_available('RICORDO-rdfstore-webservice',
+        'http://127.0.0.1:8080/ricordo-rdfstore-ws/')
+
+
+@unittest.skipUnless(owlkb_test_available(),
+        "OWLKB Webservice is not running.")
+class LiveOwlClientTestCase(unittest.TestCase):
+    """
+    Test cases that requires Tomcat running with the RICORDO owlkb
+    webservices installed and running with the Gene Ontology (go.owl)
+    defined as the actilve knowledgebase.
+    """
+
+    def setUp(self):
+        self.client = OwlkbClient()
+
+    def test_query(self):
+        results = self.client.query_terms('GO_0005886')
+        self.assertEqual(results['terms']['count'], 3)
+
+    def test_regulates_some_query(self):
+        results = self.client.query_terms('regulates some GO_0005978')
+        self.assertEqual(results.keys(), ['terms'])
+
+
+@unittest.skipUnless(rdfstore_test_available(),
+        "Ricordo RDFStore endpoint not running.")
+class LiveRicordoClientTestCase(unittest.TestCase):
+    """
+    Test cases that requires Tomcat running with the RICORDO owlkb
+    webservices installed and running with the Gene Ontology (go.owl)
+    defined as the actilve knowledgebase.
+    """
+
+    def setUp(self):
+        self.client = RdfStoreClient()
+
+    def test_search_getResourceForAnnotation(self):
+        results = self.client.getResourceForAnnotation({'query':
+            'http://identifiers.org/obo.go/GO:0031594',
+        })
+        self.assertTrue(results['resources']['count'] > 0)
+
+    def test_search_getAnnotationOfResource(self):
+        results = self.client.getAnnotationOfResource({'query':
+            'http://www.ebi.ac.uk/ricordo/toolbox/sbmlo#BIOMD0000000002_comp1',
+        })
+        p, v = results['resources']['resources'][0]['value']
+        self.assertEqual(p, 'http://biomodels.net/biology-qualifiers#is')
+        self.assertEqual(v, 'http://identifiers.org/obo.go/GO:0031594')
