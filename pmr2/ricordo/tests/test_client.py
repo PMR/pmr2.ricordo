294df21be25b79afaea39862a362a738b56b8b82
Tommy Yu <tommy.yu@auckland.ac.nz>
2014-02-24 12:42:26 +1300

    Move some code to base test module.

--- pmr2/ricordo/tests/test_client.py
+++ pmr2/ricordo/tests/test_client.py
@@ -4,28 +4,9 @@ import json
 
 from pmr2.ricordo import client
 
-
-def test_available(text, url):
-    try:
-        r = requests.get('http://127.0.0.1:8080/ricordo-owlkb-ws/')
-        return 'RICORDO-owlkb-webservice' in r.text
-    except:
-        return False
-
-def owlkb_test_available():
-    return test_available('RICORDO-owlkb-webservice',
-        'http://127.0.0.1:8080/ricordo-owlkb-ws/')
-
-def rdfstore_test_available():
-    return test_available('RICORDO-rdfstore-webservice',
-        'http://127.0.0.1:8080/ricordo-rdfstore-ws/')
-
-def virtuoso_test_available():
-    try:
-        r = requests.get('http://127.0.0.1:8890/sparql/')
-        return 'Virtuoso SPARQL' in r.text
-    except:
-        return False
+from .base import owlkb_test_available
+from .base import rdfstore_test_available
+from .base import virtuoso_test_available
 
 
 @unittest.skipUnless(owlkb_test_available(),
