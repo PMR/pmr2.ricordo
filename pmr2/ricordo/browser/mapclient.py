ceb5dd54539eb318e3f8f976a6dba09d324f40e6
Tommy Yu <tommy.yu@auckland.ac.nz>
2014-06-26 19:04:16 +1200

    A prototype of a working search form, sort of.
    - For the MAP Client and against the prototype ontology that it uses.
    - Also this has a lot of copied code from the main one but done somewhat
      differently.  Will need merging back.

--- pmr2/ricordo/browser/mapclient.py
+++ pmr2/ricordo/browser/mapclient.py
@@ -17,6 +17,8 @@ from pmr2.z3cform import page
 
 from pmr2.app.settings.interfaces import IPMR2GlobalSettings
 from pmr2.virtuoso.interfaces import IEngine
+from pmr2.virtuoso.client import SparqlClient
+from pmr2.virtuoso.sparql import quote_iri
 
 from pmr2.ricordo.interfaces import IRicordoConfig
 from pmr2.ricordo.converter import purlobo_to_identifiers
@@ -24,6 +26,79 @@ from pmr2.ricordo.converter import identifiers_to_purlobo
 from pmr2.ricordo.engine import Search
 
 from .form import BaseTermForm
+
+
+def autowildcard(tokens, fmtstr, **kw):
+    try:
+        result = fmtstr.format(**kw)
+    except KeyError as e:
+        token = kw[e[0]] = '?' + e[0]
+        tokens.append(token)
+        result = autowildcard(tokens, fmtstr, **kw)
+    return result
+
+
+class MAPClientSearch(Search):
+    """
+    MAPClient specific extensions
+    """
+
+    # TODO roll the improved dynamic query construction back into the
+    # parent class when things have stablized.
+
+    _root_query = "SELECT ?g {tokens} WHERE {{ GRAPH ?g {{ {queries} }} }}"
+
+    _query_templates = {
+        "workflow_predicate":
+            "{token} <http://physiomeproject.org/workflow/1.0/rdf-schema#"
+                 "{workflow_predicate}> {ontological_term} .",
+        "workflow_object":
+            "{token} <http://www.w3.org/2000/01/rdf-schema#subClassOf> "
+                 "<http://physiomeproject.org/workflow/1.0/rdf-schema#"
+                 "{workflow_object}> .",
+    }
+
+    def build_query(self, **kw):
+        # XXX dict comprehension not 2.6 compat
+        # escape all value strings.
+        kwords = {k: quote_iri(v) for k, v in kw.items() if v}
+        queries = []
+        tokens = []
+        for k, v in sorted(self._query_templates.items()):
+            if kwords.get(k) is None:
+                continue
+            token = '?' + k
+            tokens.append(token)
+            q = autowildcard(tokens, v, **kwords)
+            queries.append(q)
+        if not tokens:
+            return None
+        return self._root_query.format(
+            tokens=' '.join(tokens), queries='\n'.join(queries))
+
+    def search(self, **kw):
+        ontological_term = kw.pop('ontological_term')
+        if not ontological_term:
+            # simple case.
+            q = self.build_query(**kw)
+            # "pretend" this is the case...
+            return [(
+                '(All Terms)',
+                self.owls.query(q).get('results', {}).get('bindings'),
+            )]
+
+        # expand ontological term into all relevant things.
+
+        results = []
+        for term in self._generate_terms(ontological_term):
+            kw['ontological_term'] = '<%s>' % quote_iri(term)
+            q = self.build_query(**kw)
+            results.append((
+                term, 
+                self.owls.query(q).get('results', {}).get('bindings'),
+            ))
+
+        return results
 
 
 class IQueryForm(zope.interface.Interface):
@@ -48,9 +123,9 @@ class IQueryForm(zope.interface.Interface):
         required=False,
     )
 
-    ontological_term  = zope.schema.TextLine(
+    ontological_term = zope.schema.TextLine(
         title=u'Ontological term',
-        description=u'The ontological object for the relationship',
+        description=u'The ontological term for the workflow relationship.',
         required=False,
     )
 
@@ -59,12 +134,16 @@ class QueryForm(form.PostForm):
 
     fields = z3c.form.field.Fields(IQueryForm)
     ignoreContext = True
+    template = ViewPageTemplateFile('map_query_form.pt')
 
-    results = ()
+    _results = ()
 
     def update(self):
         super(QueryForm, self).update()
         self.request['disable_border'] = 1
+
+    # XXX Much is copied from form.py.  Create a common class/module
+    # when the time is right.
 
     @z3c.form.button.buttonAndHandler(u'Search', name='search')
     def handleSearch(self, action):
@@ -77,4 +156,56 @@ class QueryForm(form.PostForm):
             self.status = self.formErrorsMessage
             return
 
-        self.results = []
+        gs = zope.component.getUtility(IPMR2GlobalSettings)
+        settings = zope.component.getAdapter(gs, name='pmr2_virtuoso')
+        self.graph_prefix = settings.graph_prefix
+
+        portal = getToolByName(self.context, 'portal_url').getPortalObject()
+        self.portal_url = portal.absolute_url()
+        self.portal_catalog = getToolByName(self.context, 'portal_catalog')
+
+        ricordo_config = zope.component.queryAdapter(portal, IRicordoConfig)
+        owl_urls = ricordo_config is not None and ricordo_config.owl_urls or ()
+
+        self.engine = MAPClientSearch(
+            owlkb_graphs=owl_urls,
+            owlkb_rdfstore_uri_map=purlobo_to_identifiers,
+            rdfstore_owlkb_uri_map=identifiers_to_purlobo,
+            sparql_endpoint=settings.sparql_endpoint,
+        )
+
+        self._results = self.engine.search(**data)
+
+    def resolve_obj(self, graph_iri):
+        brain = self.portal_catalog(path=graph_iri.replace(
+            self.graph_prefix, '', 1))
+        if brain:
+            return brain[0]
+        return {}
+
+    def _process_items(self, items):
+        for i in items:
+            if not i['g']['value'].startswith(self.graph_prefix):
+                continue
+
+            obj = self.resolve_obj(i['g']['value'])
+            d = {k: v['value'] for k, v in i.items() if not k == 'g'}
+            d.update({
+                'source': obj.getURL(),
+                'obj': obj,
+            })
+            yield d
+
+    def results(self):
+        for term_url, items in self._results:
+            #url = item[
+            label = self.engine.get_owl_url_label(term_url)
+            if not label:
+                # assume the term url is actually text.
+                label, term_url = term_url, ''
+
+            yield {
+                'label': label,
+                'label_src': term_url,
+                'items': self._process_items(items),
+            }
