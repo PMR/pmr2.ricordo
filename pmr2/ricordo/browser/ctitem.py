13f780e40bcdc1152396fb2a1ecc702e2cf22bd0
Tommy Yu <tommy.yu@auckland.ac.nz>
2014-08-01 16:33:49 +1200

    Refining the rendering of workspace result.

--- pmr2/ricordo/browser/ctitem.py
+++ pmr2/ricordo/browser/ctitem.py
@@ -3,11 +3,14 @@
 from urlparse import urlparse
 from urlparse import urljoin
 
+from zope.component import queryAdapter
 from zope.component.hooks import getSite
 import zope.interface
 from zope.publisher.browser import BrowserView
 
 from Products.CMFCore.utils import getToolByName
+
+from pmr2.app.workspace.interfaces import IStorage
 
 from pmr2.ricordo.interfaces import IQRItem
 from pmr2.ricordo.browser.templates import ViewPageTemplateFile
@@ -71,4 +74,14 @@ class Workspace(QRItemView):
     index = ViewPageTemplateFile('qr_workspace.pt')
 
     def update(self):
-        pass
+        obj = self.context.get('obj')
+        if not obj:
+            return
+        # resolve the full path
+        workspace = obj.getObject()
+        storage = queryAdapter(workspace, IStorage)
+        if not storage:
+            # XXX handle all these missing things?
+            return
+        self.fileurl = '%s/@@file/%s/%s' % (
+            obj.getPath(), storage.rev, self.context['value'])
